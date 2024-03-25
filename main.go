2a40d95c67856317b9a6941b0c7a489cabcdf7e2
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-03-25 13:26:14 -0400

    feat: add base html template and basic route

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+	"html/template"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-    fmt.Println("RUN PROGRAM ----------")
+	fmt.Println("RUN PROGRAM on Port 1313 ----------")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		tmpl, _ := template.New("").ParseFiles("templates/index.html")
+		tmpl.ExecuteTemplate(w, "base", nil)
+	})
+	http.ListenAndServe(":1313", r)
 }
-
